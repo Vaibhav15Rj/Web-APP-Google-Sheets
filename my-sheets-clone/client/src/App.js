3d740c0fe143dabdef601c1ad225bdc8d286e45d
Vaibhav15Rj <baibhavroy15@gmail.com>
2025-01-12 02:29:18 +0530

    Fix start script in package.json

--- my-sheets-clone/client/src/App.js
+++ my-sheets-clone/client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Spreadsheet from './Spreadsheet';
 
 const App = () => {
   return (
     <div>
       <h1>Spreadsheet App</h1>
+      {/* This will render the Spreadsheet component within your main app */}
+      <Spreadsheet />
     </div>
   );
 };
